dd0e9773850d25e4511a0e7ecd7cc6e34930f7d1
Javier Lancha Vázquez <javier.lancha@gmail.com>
2025-09-26 22:13:54 +0200

    Complete 2022::06

---
+++ src/year2022/day6.rs
@@ -0,0 +1,63 @@
+use anyhow::Result;
+
+pub(crate) fn part1(input: &str) -> Result<String> {
+    let index = detect_marker::<4>(input).expect("Expected a solution");
+    Ok(index.to_string())
+}
+
+pub(crate) fn part2(input: &str) -> Result<String> {
+    let index = detect_marker::<14>(input).expect("Expected a solution");
+    Ok(index.to_string())
+}
+
+fn detect_marker<const MARKER_LEN: usize>(input: &str) -> Option<usize> {
+    let input_len = input.len();
+    if input_len < MARKER_LEN {
+        return None;
+    }
+
+    for i in 0..input_len - MARKER_LEN {
+        let mut substr: Vec<char> = input[i..i + MARKER_LEN].chars().collect();
+        substr.sort_unstable();
+        substr.dedup();
+
+        if substr.len() == MARKER_LEN {
+            return Some(i + MARKER_LEN);
+        }
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod test {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+    #[case("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    #[case("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    #[case("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    #[case("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    fn test_part1(#[case] input: &str, #[case] expected: usize) -> Result<()> {
+        let solution = part1(input)?;
+        assert_eq!(solution, expected.to_string());
+
+        Ok(())
+    }
+
+    #[rstest]
+    #[case("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+    #[case("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+    #[case("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+    #[case("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+    #[case("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+    fn test_part2(#[case] input: &str, #[case] expected: usize) -> Result<()> {
+        let solution = part2(input)?;
+        assert_eq!(solution, expected.to_string());
+
+        Ok(())
+    }
+}
--- src/year2022/mod.rs
+++ src/year2022/mod.rs
@@ -5,6 +5,7 @@ pub(crate) mod day2;
 pub(crate) mod day3;
 pub(crate) mod day4;
 pub(crate) mod day5;
+pub(crate) mod day6;
 
 pub fn get_solution(day: u8) -> Option<DailySolutions> {
     match day {
@@ -13,6 +14,7 @@ pub fn get_solution(day: u8) -> Option<DailySolutions> {
         3 => Some((day3::part1, day3::part2)),
         4 => Some((day4::part1, day4::part2)),
         5 => Some((day5::part1, day5::part2)),
+        6 => Some((day6::part1, day6::part2)),
         _ => None,
     }
 }
