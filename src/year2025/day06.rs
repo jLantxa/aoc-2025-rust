79adfbf76df041fd400225a56318e392676ac335
Javier Lancha Vázquez <javier.lancha@gmail.com>
2025-12-06 22:59:31 +0100

    2025::06 complete
    Terrible migraine. Stupid cephalopods.

--- src/year2025/day06.rs
+++ src/year2025/day06.rs
@@ -1,11 +1,7 @@
 use anyhow::{Result, bail};
 
 pub(crate) fn part1(input: &str) -> Result<String> {
-    let problems = parse_problems(input)?;
-    let total = problems
-        .into_iter()
-        .map(|problem| problem.op.operate_on_slice(&problem.numbers))
-        .sum::<u64>();
+    let total = process_humanly(input)?;
     Ok(total.to_string())
 }
 
@@ -36,13 +32,6 @@ impl Op {
 
         acc
     }
-
-    fn operate(&self, nums: (u64, u64)) -> u64 {
-        match self {
-            Op::Add => nums.0 + nums.1,
-            Op::Mul => nums.0 * nums.1,
-        }
-    }
 }
 
 #[derive(Debug)]
@@ -51,7 +40,8 @@ struct Problem {
     op: Op,
 }
 
-fn parse_problems(input: &str) -> Result<Vec<Problem>> {
+/// Parse the problems as a normal person would do.
+fn process_humanly(input: &str) -> Result<u64> {
     let mut problems = Vec::new();
 
     let mut lines_rev = input.trim().lines().rev();
@@ -77,22 +67,71 @@ fn parse_problems(input: &str) -> Result<Vec<Problem>> {
         }
     }
 
-    Ok(problems)
+    Ok(problems
+        .into_iter()
+        .map(|problem| problem.op.operate_on_slice(&problem.numbers))
+        .sum::<u64>())
 }
 
+/// Process the math problems from right to left, as cephalopods do.
+/// Yes, it is weird.
+///
+/// Apparently, parsing from right-to-left has equal or better performance.
 fn process_cephalopodly(input: &str) -> Result<u64> {
-    todo!()
+    let lines: Vec<_> = input.lines().collect();
+    let line_len = lines[0].len();
+    let rev_lines: Vec<Vec<char>> = lines
+        .into_iter()
+        .map(|line| line.chars().rev().collect::<Vec<char>>())
+        .collect();
+    let num_lines = rev_lines.len();
+
+    let mut acc: u64 = 0;
+    let mut stack = Vec::new();
+
+    for i in 0..line_len {
+        let mut chs: Vec<char> = Vec::with_capacity(num_lines);
+        let mut is_empty_line = true;
+
+        for rev_line in rev_lines.iter().take(num_lines) {
+            let ch = rev_line[i];
+            if ch != ' ' {
+                is_empty_line = false;
+            }
+            chs.push(ch);
+        }
+
+        if is_empty_line {
+            continue;
+        }
+
+        let num_str: String = chs[0..num_lines - 1].iter().collect();
+        let num: u64 = num_str.trim().parse().expect("Should be a number");
+        stack.push(num);
+
+        let op_ch = chs[num_lines - 1];
+        match op_ch {
+            '+' => {
+                acc += Op::Add.operate_on_slice(&stack);
+                stack.clear();
+            }
+            '*' => {
+                acc += Op::Mul.operate_on_slice(&stack);
+                stack.clear();
+            }
+            ' ' => (),
+            _ => bail!("Unhandled operator"),
+        }
+    }
+
+    Ok(acc)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "\
-123 328  51 64
- 45 64  387 23
-  6 98  215 314
-*   +   *   +  ";
+    const INPUT: &str = "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  ";
 
     #[test]
     fn test_part1() -> Result<()> {
